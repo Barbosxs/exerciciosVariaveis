b601774011867fddf7dd097dc1a636e16bd66c0d
Barbosxs <djubanski.julia@gmail.com>
2025-05-23 14:16:46 +0000

    fim

---
+++ ATIVIDADE.js
@@ -0,0 +1,48 @@
+// 1
+let nome = "Julia";
+console.log("Olá, meu nome é " + nome + ".");
+
+// 2
+let idade = 16;
+let cidade = "Almirante Tamandaré";
+console.log("Tenho " + idade + " anos e moro em " + cidade + ".");
+
+// 3
+let comida1 = "lasanha";
+let comida2 = "pizza";
+let comida3 = "sushi";
+console.log("Minhas comidas favoritas são: " + comida1 + ", " + comida2 + " e " + comida3 + ".");
+
+// 4
+let preco = 49.90;
+console.log("O produto custa R$ " + preco + ".");
+
+// 5
+let produto = "caderno";
+let quantidade = 3;
+console.log("Comprei " + quantidade + " unidades de " + produto + ".");
+
+// 6
+let temDesconto = true;
+console.log("Tem desconto disponível? " + temDesconto);
+
+// 7
+let anoAtual = 2025;
+let anoNascimento = 2008;
+let idadeCalculada = anoAtual - anoNascimento;
+console.log("Você tem " + idadeCalculada + " anos.");
+
+// 8
+let curso = "Desenvolvimento de Sistemas";
+let instituicao = "SENAI";
+console.log("Estou cursando " + curso + " no " + instituicao + ".");
+
+// 9
+let cor1 = "azul";
+let cor2 = "roxo";
+let cor3 = "preto";
+console.log("Minhas cores preferidas são " + cor1 + ", " + cor2 + " e " + cor3 + ".");
+
+// 10
+let mensagem = "Aprender programação é divertido!";
+console.log(mensagem);
